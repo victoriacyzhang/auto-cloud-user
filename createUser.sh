150d55981e3b3b6e868ec64dd67a44f0fe794a36
Victoria <victoriacyzhang@gmail.com>
2018-11-04 18:18:13 -0800

    done

--- createUser.sh
+++ createUser.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#----- Step 1: create new user
 # if not root
 if [[ $UID -ne 0 ]]; then echo "Please run $0 as root with sudo command." && exit 1; fi
 
@@ -37,36 +38,46 @@ createhomedir -c -u $USERNAME
 
 echo "Done creating the user."
 
----------------------------------
+#----- Step 2: generate new pair of ssh key
 
 echo "Now generating the ssh key."
 echo "new" | ssh-keygen -t rsa
 
----------------------------------
+#----- step 3: store ssh key in user
 
 echo "Now storing ssh key."
 
-# need to create .ssh
+# create .ssh folder
 sudo mkdir /Users/$USERNAME/.ssh
-# need to create authorized_keys
+
+# create authorized_keys file
 sudo touch /Users/$USERNAME/.ssh/authorized_keys
-# need to change permission of .ssh to 700
+
+# set permission of .ssh to 700
 sudo chmod 700 /Users/$USERNAME/.ssh
-# need to change permission of authorized_keys to 600
-sudo chmod 600 /Users/$USERNAME/.ssh/authorized_keys
+
+# set permission of authorized_keys to 644
+sudo chmod 644 /Users/$USERNAME/.ssh/authorized_keys
+
 # copy content of new.pub to authorized_keys
 sudo cp ~/auto-cloud-user/new.pub /Users/$USERNAME/.ssh/authorized_keys
 
---------------------------------
+# copy private key to desktop
+sudo cp ~/auto-cloud-user/new ~/Desktop/new
+sudo chmod 555 ~/Desktop/new
+
+#----- Step 4: link user with ssh key
 
 echo "Now linking ssh to cloud"
 
 echo "Please enter your name in the format of lastname_firstname."
 read FOLDERNAME
 
-# change to active directory
+# make folder in cloud directory
 sudo mkdir /Volumes/pegasus_R6/data/ftp_server/active/$FOLDERNAME
 
-sudo chown $USERNAME /Volumes/pegasus_R6/data/ftp_server/active/$FOLDERNAME
+# linking
+sudo chown -R $USERNAME /Volumes/pegasus_R6/data/ftp_server/active/$FOLDERNAME
+sudo chown -R $USERNAME /Users/$USERNAME
 
 echo "Done."
